4d8114534434b23fc9cd20e3c813648806834516
Fisab <vladsudakov5@gmail.com>
2017-09-18 23:58:50 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/fisab/control/MainActivity.java
+++ app/src/main/java/com/example/fisab/control/MainActivity.java
@@ -2,37 +2,42 @@ package com.example.fisab.control;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    public TextView log;
+    private void setButtonListener(final Button btn, final int typeButton){
+        final int PLAY = 0;
+        final int MUTE = 1;
+        final int UNMUTE = 2;
 
-
-    public android.view.View.OnClickListener myButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Requests r = new Requests(log);
-            r.get("http://192.168.1.65:5000/mute", 5000);
-            r.get_text();
-        }
-    };
-
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            Requests r = new Requests();
+            @Override
+            public void onClick(View v) {
+                switch (typeButton){
+                    case PLAY:
+                        r.get("http://192.168.0.12:5000/play", 5000);
+                    case MUTE:
+                        r.get("http://192.168.0.12:5000/mute", 5000);
+                    case UNMUTE:
+                        r.get("http://192.168.0.12:5000/unmute", 5000);
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button3);
-        button.setOnClickListener(myButtonClickListener);
-        log = (TextView) findViewById(R.id.textView);
 
+        Button play = (Button) findViewById(R.id.play);
+        Button mute = (Button) findViewById(R.id.mute);
+        Button unmute = (Button) findViewById(R.id.unmute);
 
+        setButtonListener(play, 0);
+        setButtonListener(mute, 1);
+        setButtonListener(unmute, 2);
     }
 }
